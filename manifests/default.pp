bfc9e89d8df30ed7e8e3605dfd940bd6c60b183f
alonisser <alonisser@gmail.com>
2013-07-20 16:14:14 +0300

    added postgresql module, still figuring this out

--- manifests/default.pp
+++ manifests/default.pp
@@ -16,26 +16,26 @@ package {
 	["python-lxml","python-imaging"]:
 	ensure => present,
 }
-#class { 'apt':
 
-#		}
-#include apt
+class{ 'apt':}
 
-#class{ 'apt':} -> apt::builddep { ["python-imaging","python-lxml"]:
-#	require => Class['apt']	
-# }
+include apt
 
-#class {'nodejs':
+apt::builddep { ["python-imaging","python-lxml"]:
+		
+ }
 
-#}
-#include nodejs
+class {'nodejs':
 
-#package {"less":
-#	ensure => present,
-#	provider => 'npm',
-#	require => Package['npm'],
+}
+include nodejs
 
-#}
+package {"less":
+	ensure => present,
+	provider => 'npm',
+	require => Package['npm'],
+
+}
 
 package {"python-virtualenv":
 	ensure => present,
@@ -47,20 +47,35 @@ package {"sphinx":
 	require => Package["python-pip"],
 	provider => pip
 }
-#service {'apache2':
-#	ensure => running,
-#	enable => true,
-#	subscribe => File["/etc/apache2/apache2.conf"]
-#}
-# a comment 
-class mysql{
-	package {
-	["mysql-server", "mysql-client"]:
-	ensure => present,
-	}
-#	service {"mysql-server":
-#	ensure => running,
-#	}
+service {'apache2':
+	ensure => running,
+	enable => true,
+	#subscribe => File["/etc/apache2/apache2.conf"]
 }
-#the mysql class won't work if we don't include
-include mysql
+
+#include postgresql
+class { 'postgresql':
+  charset => 'UTF8',
+}-> class { 'postgresql::server':
+config_hash => {
+    'ip_mask_deny_postgres_user' => '0.0.0.0/32',
+    'ip_mask_allow_all_users'    => '0.0.0.0/0',
+    'listen_addresses'           => '*',
+  },
+}
+
+class {'postgresql::client':
+	package_ensure => present,
+	}
+class {'postgresql::python':
+	package_ensure => present,
+	}
+
+postgresql::db { 'Oknessetdb':
+  user     => 'Oknessetdevuser',
+  password => 'devuserpassword', #change this after the setup 
+  require => Class["postgresql::server"]
+}
+
+# a comment 
+
