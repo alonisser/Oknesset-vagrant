b6dda598d417f185eb8cb0d88f48566e754ad40f
alonisser <alonisser@gmail.com>
2013-08-19 01:47:27 +0300

    A working prototype, most python tools already there, expect for postgresql and the oknesset installation itself

--- manifests/default.pp
+++ manifests/default.pp
@@ -1,31 +1,36 @@
 # Run apt-get update when anything beneath /etc/apt/ changes
-#taken from https://blog.kumina.nl/2010/11/puppet-tipstricks-running-apt-get-update-only-when-needed/
-#exec { "apt-get update":
-#command => "/usr/bin/apt-get update",
-#onlyif => "/bin/sh -c '[ ! -f /var/cache/apt/pkgcache.bin ] || /usr/bin/find /etc/apt/* -cnewer /var/cache/apt/pkgcache.bin | /bin/grep . > /dev/null'",
-#}
+#taken from https://blog.kumina.nl/2010/11/puppet-tipstricks-running-apt-get-update-only-#when-needed/
+exec { "apt-get update":
+command => "/usr/bin/apt-get update",
+onlyif => "/bin/sh -c '[ ! -f /var/cache/apt/pkgcache.bin ] || /usr/bin/find /etc/apt/* -cnewer /var/cache/apt/pkgcache.bin | /bin/grep . > /dev/null'",
+}
 
 package {
 	["build-essential","apache2","git","python","python-dev","python-setuptools", "python-pip"]:
 	ensure => present,
-	require => Class["apt"],
+	require => [Class["apt"], Exec["apt-get update"]],
 	}
 
-# include apt
-class {"apt":
-	always_apt_update => true,
-}
+include apt
 
 apt::builddep { ["python-imaging","python-lxml"]:
-		
+		require => Exec["apt-get update"],
  }
-
-include nodejs
-
-package {"less":
-	ensure => present,
-	provider => 'npm',
-	require => Package['npm'],
+#The node.js from:http://theholyjava.wordpress.com/2013/06/21/installing-latest-node-js-and-npm-modules-with-puppet/
+class prepare {
+  apt::ppa { 'ppa:chris-lea/node.js': }
+}
+include prepare
+ 
+package {'nodejs':
+  ensure => present,
+  require => Class['prepare'],
+}
+ 
+package {['grunt-cli','less']:
+    ensure   => present,
+    provider => 'npm',
+    require  => Package['nodejs'],
 }
 
 package {"python-virtualenv":
@@ -42,30 +47,31 @@ package {"sphinx":
 service {'apache2':
 	ensure => running,
 	enable => true,
+	require => Package["apache2"]
 	#subscribe => File["/etc/apache2/apache2.conf"]
 }
 
 #include postgresql
-class { 'postgresql':
-  charset => 'UTF8',
-  require => Class['apt'],
-}-> class { 'postgresql::server':
+# class { 'postgresql':
+#   charset => 'UTF8',
+#   require => Class['apt'],
+# }-> class { 'postgresql::server':
 
-config_hash => {
-    'ip_mask_deny_postgres_user' => '0.0.0.0/32',
-    'ip_mask_allow_all_users'    => '0.0.0.0/0',
-    'listen_addresses'           => '*',
-  },
-}
+# config_hash => {
+#     'ip_mask_deny_postgres_user' => '0.0.0.0/32',
+#     'ip_mask_allow_all_users'    => '0.0.0.0/0',
+#     'listen_addresses'           => '*',
+#   },
+# }
 
-class {'postgresql::client':
-	package_ensure => present,
-	require => Class['postgresql'],
-	}
-class {'postgresql::python':
-	package_ensure => present,
-	require => Class['postgresql'],
-	}
+# class {'postgresql::client':
+# 	package_ensure => present,
+# 	require => Class['postgresql'],
+# 	}
+# class {'postgresql::python':
+# 	package_ensure => present,
+# 	require => Class['postgresql'],
+# 	}
 
 #postgresql::db { 'Oknessetdb':
 #  user     => 'Oknessetdevuser',
@@ -75,3 +81,11 @@ class {'postgresql::python':
 
 # a comment 
 
+# python::requirements { '/home/Open-Knesset/requirements.txt':
+#   virtualenv => '/home/Open-Knesset',
+# }
+
+# python::virtualenv { '/home/Open-Knesset':
+#   ensure       => present,
+#   requirements => '/home/Open-Knesset/requirements.txt',
+# }
